4bfb46db637286cfac5a1c723cc1feece634c9ad
Anton Lukin <anton@lukin.me>
2025-05-25 12:45:27 +0200

    Update common assets

--- functions.php
+++ functions.php
@@ -10,17 +10,27 @@ if (!defined('WPINC')) {
 require_once get_stylesheet_directory() . '/modules/video.php';
 require_once get_stylesheet_directory() . '/modules/tweaks.php';
 require_once get_stylesheet_directory() . '/modules/live.php';
-
 /**
  * Add custom styles
  */
 add_action ('wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'lukin-blog', get_stylesheet_directory_uri() . '/assets/styles.css', '1.1' );
+    wp_enqueue_style(
+        'lukin-blog',
+        get_stylesheet_directory_uri() . '/assets/common.css',
+        array(),
+        filemtime( get_stylesheet_directory() . '/assets/common.css' )
+    );
 }, 20 );
 
 /**
  * Add custom scripts
  */
 add_action ('wp_enqueue_scripts', function() {
-    wp_enqueue_script( 'lukin-blog', get_stylesheet_directory_uri() . '/assets/scripts.js', array(), '1.1', true );
+    wp_enqueue_script(
+        'lukin-blog',
+        get_stylesheet_directory_uri() . '/assets/common.js',
+        array(),
+        filemtime( get_stylesheet_directory() . '/assets/common.js' ),
+        true
+    );
 }, 20 );
